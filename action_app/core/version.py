52099aa3ea23504797d74e27a8df5754d520efb9
William McCumsite <william.mccumstie@gmail.com>
2020-02-13 11:15:52 +0000

    Bump version for 0.2.0c0 release

--- action_app/core/version.py
+++ action_app/core/version.py
@@ -27,7 +27,7 @@
 
 from cement.utils.version import get_version as cement_get_version
 
-VERSION = (0, 2, 0, 'alpha', 0)
+VERSION = (0, 2, 0, 'rc', 0)
 
 def get_version(version=VERSION):
     return cement_get_version(version)
